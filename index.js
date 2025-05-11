3b3f2d9429dbd4a32a27411e25affb3e18f1d893
josueCarreno <150408018+josueCarreno@users.noreply.github.com>
2025-05-10 22:42:51 -0400

    10-05-25

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 //Proyecto 2: Juego de Tres en Raya (Tic-Tac-Toe)
 //Crea un juego de tres en raya que permita a dos jugadores jugar por turnos.
-function empezar(){
-  //alert("Bienvenido a el Juego de Tres en Raya")
-  let arreglo = [ [ ["  "],["  "],["  "] ], [ ["  "],["  "],["  "] ], [ ["  "],["  "],["  "] ]  ];
-  console.log(arreglo[0][0]);
-  alert(`${arreglo[0][0]} | ${arreglo[0][1]} | ${arreglo[0][2]} \n--------\n${arreglo[1][0]} | ${arreglo[1][1]} | ${arreglo[1][2]} \n--------\n${arreglo[2][0]} | ${arreglo[2][1]} | ${arreglo[2][2]} `);
+function empezar(arreglo = [ [ ["  "],["  "],["  "] ], [ ["  "],["  "],["  "] ], [ ["  "],["  "],["  "] ]  ], contador = 0, sumaX = 0){
+  if (contador === 0) {
+    alert("Bienvenido a el Juego de Tres en Raya");
+  }
+  let imprimirFila = "Por Favor ingrese la posición de la fila: \n\n";
+  let imprimirColumna = "Ahora ingrese la posición de la columna: \n\n";
+  let imprimirJuego = `${arreglo[0][0]} | ${arreglo[0][1]} | ${arreglo[0][2]} \n--------\n${arreglo[1][0]} | ${arreglo[1][1]} | ${arreglo[1][2]} \n--------\n${arreglo[2][0]} | ${arreglo[2][1]} | ${arreglo[2][2]} `;
+  let fila = prompt(imprimirFila + imprimirJuego);
+  let columna = prompt(imprimirColumna + imprimirJuego);
+  fila =+ parseInt(fila);
+  columna =+ parseInt(columna);
+  let marca;
+  if (contador % 2 === 0) {
+    marca = "X";
+  } 
+  else {
+    marca = "O"
+  }
+  arreglo[fila][columna] = marca;
+  if (contador >= 4) {
+      arreglo[1].map(
+        (caja) => {
+          if (caja == marca) {
+            sumaX = sumaX + 1;
+          }
+        }
+      )
+    }
+  if (sumaX === 3) {
+    alert("Ganó la " + marca);
+  }
+  empezar(arreglo, contador + 1);
 }
 
 empezar();
--- prueba.js
+++ prueba.js
@@ -1,4 +1,5 @@
 //idea: pedir primero número de fila, osea posición y luego hacer otro prompt para pedir número de columna
+//despues aomodar para que sea mas comodo para el jugador al seleccionar las posiciones que le reste 1...
 
 arreglo = [ [ [],[],[] ], [ [],[],[] ], [ [],[],[] ]  ];
 
@@ -13,3 +14,23 @@ console.log(arreglo[2]);
 if ( arreglo[0][0] == "X" && arreglo[1][0] == "X" && arreglo[2][0] == "X") {
   console.log("Victoría");
 }
+
+
+    function recorrerFila(sumaX, contador = 0) {
+      if (contador >= arreglo[0].length) {
+        return sumaX;
+      }
+      arreglo[contador].map(
+        (caja) => {
+          if (caja == marca) {
+            sumaX = sumaX + 1;
+          }
+          if (sumaX === 3) {
+            alert("Ganó la " + marca);
+            return contador = 3;
+          }
+        }
+      )
+      recorrerFila(sumaX, contador + 1);
+    }
+    recorrerFila(sumaX);
